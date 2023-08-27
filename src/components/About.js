7b976146fb1c3f6b9818e5cdd42ee829e14c74bc
onjorobrian <onjorobrian1@gmail.com>
2023-08-28 01:52:30 +0300

    assignmentdone

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="About">
+    <h2>About me</h2>
+    <p>Assignment</p>
+    <img src={image} alt=  "I made this"></img>
+  </div>;
 }
 
 export default About;
